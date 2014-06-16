8ca71a973f794181b53f5a0d127b1db1476ab17c
John Doe <john.doe@yahoo.com>
2014-06-15 21:41:23 -0700

    Have dual pages working.

--- pururinparser.py
+++ pururinparser.py
@@ -5,60 +5,93 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from os import path, makedirs
 from string import rfind
+#import wget
 
 def doIt():
 	driver = webdriver.Firefox()
 	driver.get(options.startURL)
 
+	# Parse page count from label
+	# ofPagesElement = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[2]/span/label')
+	# ofPagesText = ofPagesElement.text
+	# print ofPagesText
+	# ofPagesNumber = int(ofPagesText[rfind(ofPagesText)+1:])
+	# oddPages = bool(ofPagesNumber%2)
+
+	pagesPer = 1
+	if options.dual:	# Switch to 'western' dual page mode
+		doubleButton = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[2]/div/div[2]/div[4]/a[2]')
+		doubleButton.click()
+		pagesPer = 2
+		print "Using double page mode."
+
 	throttle = int(options.throttle)
 	pageLimit = int(options.pageLimit)
-	downloadMode = options.download
+	# downloadMode = options.download
 
 	urlString = ""
 	bookName = ""
-	
-	print "throttle %s pageLimit %s downloadMode %s" % (throttle, pageLimit, downloadMode)
+	pastB = "" # Work around for uneven pages in dual mode.
+
 	for page in range(pageLimit):
+
+		# Deal with the image element
 		try:
-			imageElement = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[2]/div/div[1]/a[2]/img')
+			imageElementA = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[2]/div/div[1]/a[2]/img[1]')
+			imageUrlA = imageElementA.get_attribute('src')
+			print imageUrlA
+			urlString += imageUrlA + "\n"
+
 		except NoSuchElementException:
-			print "Reached end for \"%s\", got through %s pages." % (bookName, page)
+			print "Reached end for \"%s\", got through %s pages." % (bookName, pagesPer*page)
 			break
 
 
-		imageUrl = imageElement.get_attribute('src')
-		print imageUrl
+		# Deal with a second image element if in dual mode
+		if options.dual:
+			try:
+				imageElementB = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[2]/div/div[1]/a[2]/img[2]')
+				imageUrlB = imageElementB.get_attribute('src')
+				if imageUrlB == pastB:
+					# When there is just one image, this exception doesn't get thrown for some reason
+					# So as a remedy, if the url is the same as the last time around, I throw it.
+					raise NoSuchElementException()	
 
+				print imageUrlB
+				urlString += imageUrlB + "\n"
+				pastB = imageUrlB
 
+			except NoSuchElementException:
+				print "Reached end for \"%s\", got through %s pages." % (bookName, (pagesPer*page)+1)
+				break
+			
+
+		# Name the book from the last '/'+1 to the last '-'
 		if len(bookName) == 0:
-			bookName = imageUrl[rfind(imageUrl, '/')+1:-6]
+			bookName = imageUrlA[rfind(imageUrlA, '/')+1:rfind(imageUrlA, '-')]
 
-		urlString += imageUrl + "\n"
-
-		
+		# Throttle the speed, otherwise Selenium will rip through pages a half-second a piece.
 		sleep(throttle)
 
+		# Click the next button.
 		nextPageButton = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[1]/a[2]')
-
 		nextPageButton.click()
-		sleep(throttle)
-
 
 	driver.quit()
-
-
-
-
+	return [urlString, bookName]
 
 parser = OptionParser()
-# parser.add_option("-f", "--file", help="The file to write", default="output.txt", metavar="FileLocation")
 parser.add_option("-s", "--startURL", help="The starting URL \"http://www.example.com/page\"", default="http://www.example.com/page", metavar="startURL")
 parser.add_option("-n", "--pageLimit", help="Only download n pages", default='9999', metavar="integer")
-parser.add_option("-d", "--download", help="Download in program", default=False, action="store_true")
+#parser.add_option("-w", "--download", help="Download in program", default=False, action="store_true")
 parser.add_option("-t", "--throttle", help="Seconds between pages", default='2', metavar="integer")
+parser.add_option("-d", "--dual", help="Dual Page Mode", default=False, action="store_true")
+parser.add_option("-l", "--list", help="List file (\\n delimit)", default='', metavar="filename")
 
 (options, args) = parser.parse_args()
 print "options:\n  %s\nargs:\n  %s" % (options, args)
 
 # Do it!
-doIt()
+returnArray = doIt()
+outfile = open(returnArray[1]+".txt", 'a')
+outfile.write(returnArray[0])
